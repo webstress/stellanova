418c7bbd1de7bcd96390614ce663445e9b55216a
infhinet1 <infhinet1@gmail.com>
2024-08-15 16:05:57 +0100

    pages

---
+++ contact.php
@@ -0,0 +1,15 @@
+<?php
+include_once(__DIR__ . './incs/head.php');
+include_once(__DIR__ . './incs/navbar.php');
+?>
+<section class="about hero">
+    <div class="container">
+        <h2>StellarNova</h2>
+        <hr>
+        <h1>CONTACT US</h1>
+    </div>
+</section>
+<?php
+include_once(__DIR__ . './incs/ab-footer.php');
+include_once(__DIR__ . './incs/script.php');
+?>
---
+++ tandc.php
@@ -0,0 +1,19 @@
+<?php
+include_once(__DIR__ . './incs/head.php');
+include_once(__DIR__ . './incs/navbar.php');
+?>
+<section class="about hero">
+    <div class="container">
+        <h2>StellarNova </h2>
+        <hr>
+        <h1>TERMS and Conditions</h1>
+    </div>
+</section>
+
+
+
+
+<?php
+include_once(__DIR__ . './incs/ab-footer.php');
+include_once(__DIR__ . './incs/script.php');
+?>
